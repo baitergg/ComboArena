595c2e761171b42016feff2623dc2af12dc9faf0
baitergg <vanapelme370@gmail.com>
2026-05-04 13:23:52 +0500

    Add Game1 and EventBus

--- ComboArena/Core/EventBus.cs
+++ ComboArena/Core/EventBus.cs
@@ -1,6 +1,105 @@
-namespace ComboArena.Core;
+using System;
+using System.Collections.Generic;
 
-public class EventBus
+namespace ComboArena.Core
 {
-    
+    public interface IEvent
+    {
+        string Type { get; }
+    }
+
+    public class EventBus
+    {
+        private static EventBus _instance;
+        public static EventBus Instance => _instance ??= new EventBus();
+
+        private readonly Dictionary<string, List<Action<IEvent>>> _listeners;
+
+        private EventBus()
+        {
+            _listeners = new Dictionary<string, List<Action<IEvent>>>();
+        }
+
+        public void Subscribe(string eventType, Action<IEvent> listener)
+        {
+            if (!_listeners.TryGetValue(eventType, out var value))
+            {
+                value = [];
+                _listeners[eventType] = value;
+            }
+
+            value.Add(listener);
+        }
+
+        public void Unsubscribe(string eventType, Action<IEvent> listener)
+        {
+            if (_listeners.TryGetValue(eventType, out var subscriber))
+            {
+                subscriber.Remove(listener);
+            }
+        }
+
+        public void Publish(IEvent evt)
+        {
+            var eventType = evt.Type;
+            if (!_listeners.TryGetValue(eventType, out var subscriber)) return;
+            var handlers = new List<Action<IEvent>>(subscriber);
+            foreach (var handler in handlers)
+                handler?.Invoke(evt);
+        }
+    }
+
+    public class PlayerMovedEvent : IEvent
+    {
+        public string Type => "PlayerMoved";
+        public float X { get; }
+        public float Y { get; }
+
+        public PlayerMovedEvent(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class EnemySpawnedEvent : IEvent
+    {
+        public string Type => "EnemySpawned";
+        public object Enemy { get; }
+
+        public EnemySpawnedEvent(object enemy)
+        {
+            Enemy = enemy;
+        }
+    }
+
+    public class AttackEvent : IEvent
+    {
+        public string Type => "Attack";
+        public object Attacker { get; }
+        public object Target { get; }
+        public float Damage { get; }
+
+        public AttackEvent(object attacker, object target, float damage)
+        {
+            Attacker = attacker;
+            Target = target;
+            Damage = damage;
+        }
+    }
+
+    public class HealthChangedEvent : IEvent
+    {
+        public string Type => "HealthChanged";
+        public object Entity { get; }
+        public float OldHealth { get; }
+        public float NewHealth { get; }
+
+        public HealthChangedEvent(object entity, float oldHealth, float newHealth)
+        {
+            Entity = entity;
+            OldHealth = oldHealth;
+            NewHealth = newHealth;
+        }
+    }
 }
--- ComboArena/Game1.cs
+++ ComboArena/Game1.cs
@@ -1,52 +1,76 @@
-﻿using Microsoft.Xna.Framework;
+﻿using ComboArena.Controller;
+using ComboArena.View;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
-namespace ComboArena;
-
-public class Game1 : Game
+namespace ComboArena
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-
-    public Game1()
+    public class Game1 : Game
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
-    }
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private GameController _controller;
+        private GameView _view;
+        private SpriteFont _font;
 
-    protected override void Initialize()
-    {
-        // TODO: Add your initialization logic here
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            
+            _graphics.PreferredBackBufferWidth = 1920;
+            _graphics.PreferredBackBufferHeight = 1080;
+            
+            _graphics.IsFullScreen = true;
+            
+            _controller = new GameController();
+        }
 
-        base.Initialize();
-    }
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+            _font = null;
+            
+            _view = new GameView(_controller);
+            _view.LoadContent(GraphicsDevice, _font);
+        }
 
-    protected override void LoadContent()
-    {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        protected override void Update(GameTime gameTime)
+        {
+            if (Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
+                Exit();
 
-        // TODO: use this.Content to load your game content here
-    }
+            _controller.Update(gameTime);
+            
+            base.Update(gameTime);
+        }
 
-    protected override void Update(GameTime gameTime)
-    {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-            Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
-
-        // TODO: Add your update logic here
-
-        base.Update(gameTime);
-    }
-
-    protected override void Draw(GameTime gameTime)
-    {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        // TODO: Add your drawing code here
-
-        base.Draw(gameTime);
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+            const float zoom = 2.0f;
+            var playerPos = _controller.Player.Position;
+            var viewport = GraphicsDevice.Viewport;
+            var screenCenter = new Vector2(viewport.Width / 2f, viewport.Height / 2f);
+            var playerCenter = playerPos + new Vector2(_controller.Player.Width / 2,
+                                                       _controller.Player.Height / 2);
+            
+            var viewMatrix = Matrix.CreateTranslation(-playerCenter.X, -playerCenter.Y, 0) *
+                             Matrix.CreateScale(zoom, zoom, 1) *
+                             Matrix.CreateTranslation(screenCenter.X, screenCenter.Y, 0);
+            
+            _spriteBatch.Begin(transformMatrix: viewMatrix);
+            _view.Draw(_spriteBatch);
+            _spriteBatch.End();
+            
+            _spriteBatch.Begin();
+            _view.DrawUI(_spriteBatch);
+            _spriteBatch.End();
+            
+            base.Draw(gameTime);
+        }
     }
 }
